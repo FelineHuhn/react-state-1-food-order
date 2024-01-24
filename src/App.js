73809694e3f4c72ddc4e5ca2144b5f0cd06bd7a0
Feline Huhn <feline.huhn@gmx.de>
2024-01-24 16:16:20 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import "./styles.css";
 
 export default function App() {
-  return <Counter />;
+  return (
+    <>
+      <h1>Place your order</h1>
+      <FoodOrder />
+    </>
+  );
 }
 
-function Counter() {
-  return <button>You have clicked this button 0 times</button>;
+function FoodOrder() {
+  const food = "?";
+  const vegan = true;
+
+  return (
+    <>
+      <div>
+        <button onClick={() => {}}>
+          <span role="img" aria-label="A hamburger">
+            🍔
+          </span>
+        </button>
+        <button onClick={() => {}}>
+          <span role="img" aria-label="A pizza">
+            🍕
+          </span>
+        </button>
+        <button onClick={() => {}}>
+          <span role="img" aria-label="A taco">
+            🌮
+          </span>
+        </button>
+      </div>
+      <p>Selected food: {food}</p>
+      <button onClick={() => {}}>
+        Make it vegan:{" "}
+        {vegan ? (
+          <span role="img" aria-label="Thumbs up">
+            👍
+          </span>
+        ) : (
+          <span role="img" aria-label="Thumbs down">
+            👎
+          </span>
+        )}
+      </button>
+    </>
+  );
 }
